1a4dbe40008b63b1b43cbd16f5a75a7e76c526dd
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-20 01:37:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def line(katz_deli)
+deli_line = []
+katz_deli.each_with_index do |person,index|
+  deli_line << "#{index+1}. #{person}"
+end
+ line = deli_line.join(" ")
+  if katz_deli.size == 0
+     puts "The line is currently empty."
+  else
+    puts "The line is currently: #{line}"
+  end
+end
+
+def take_a_number(katz_deli,person)
+  katz_deli << person
+  puts "Welcome, #{person}. You are number #{katz_deli.index(person)+1} in line."
+end
+
+def now_serving(katz_deli)
+while katz_deli.size > 0
+ puts "Currently serving #{katz_deli[0]}."
+katz_deli.shift
+ end
+
+if katz_deli.size == 0
+  puts "There is nobody waiting to be served!"
+  end
+end
--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -1,3 +1,4 @@
+
 describe 'Deli Counter' do
 
   let(:katz_deli) { [] }
@@ -68,7 +69,9 @@ describe 'Deli Counter' do
       it "should serve the first person in line and remove them from the queue" do
         expect($stdout).to receive(:puts).with("Currently serving Logan.")
         now_serving(other_deli)
+
         expect(other_deli).to eq(%w(Avi Spencer))
+
       end
     end
   end
